323a7225b68f8e6d614d309772889a25635fb23b
Madeline <mmcintosh2k14@gmail.com>
2019-09-18 19:52:50 -0400

    adds completed Stretch challenge 2 on closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -37,34 +37,33 @@ website('house');
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-function counterMaker() {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+// IMPLEMENTATION OF counterMaker:
+// 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+// 2- Declare a function `counter`. It should increment and return `count`.
+//      NOTE: This `counter` function, being nested inside `counterMaker`,
+//      "closes over" the `count` variable. It can "see" it in the parent scope!
+// 3- Return the `counter` function.
 
+const counterMaker = () => {
   let count = 0;
-  // 2- Declare a function `counter`. It should increment and return `count`.
-
-  function counter() {
-    count++; 
-    //return count;
-  };
-
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
+  return function counter(){
+    return ++count;
+  }
 };
-// Example usage: 
-//const myCounter = counterMaker();
-counterMaker(); // 1
-// myCounter(); // 2
+
+const myCounter = counterMaker();
+
+console.log(myCounter());
+console.log(myCounter());
+
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+//const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+//};
